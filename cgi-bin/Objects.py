45022ebd42170025730c7d9a036e1ff5d5a3f216
tboccali <>
2012-04-10 19:08:10 +0000

    first addition

---
+++ cgi-bin/Objects.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+from datetime import *
+cgitb.enable()
+
+from MySQLdb import *
+from storm.locals import *
+
+
+#
+# transfer
+#
+class Transfer (object):
+    __storm_table__ = "transfers"
+    TRANSFER_ID=Int(primary=True)
+    SENDER=Unicode()
+    RECEIVER=Unicode()
+    ISSUED_DATE=datetime(1970,1,1)
+    RECEIVED_DATE=date.today()
+    STATUS=Unicode()
+    COMMENT=Unicode()
+    def __init__(self, SENDER,RECEIVER, ISSUED_DATE=datetime(1970,1,1), RECEIVED_DATE=date.today(), STATUS="", COMMENT=""):
+        self.SENDER=unicode(SENDER)
+        self.RECEIVER=unicode(RECEIVER)
+        self.ISSUED_DATE=ISSUED_DATE
+        self.RECEIVED_DATE=RECEIVED_DATE
+        self.STATUS=unicode(STATUS)
+        self.COMMENT=unicode(COMMENT)
+
+#
+# Session
+#
+class Session (object):
+    __storm_table__ = "sessions"
+    SESSION_ID=Int(primary=True)
+    CENTER=Unicode()
+    OPERATOR=Unicode()
+    DATE=date.today()
+    TYPE=Unicode()
+    COMMENT=Unicode()
+    def __init__(self,SESSION_ID,CENTER,TYPE, OPERATOR,DATE=date.today(), COMMENT=""):
+        self.SESSION_ID=SESSION_ID
+        self.CENTER=unicode(CENTER)
+        self.OPERATOR=unicode(OPERATOR)
+        self.DATE=DATE
+        self.TYPE=unicode(type)
+        self.COMMENT=unicode(COMMENT)
+    
+#
+# inventory
+#
+class Roc(object):
+  __storm_table__ = "inventory_roc"
+  ROC_ID = Unicode(primary=True)
+  TRANSFER_ID = Int()
+  transfer = Reference(TRANSFER_ID, Transfer.TRANSFER_ID)
+  COMMENT = Unicode()
+  LASTTEST_ROC=Int()
+  def __init__(self,ROC_ID, TRANSFER_ID, COMMENT="",LASTTEST_ROC=0):
+      self.ROC_ID=unicode(ROC_ID)
+      self.TRANSFER_ID=(TRANSFER_ID)
+      self.COMMENT=unicode(COMMENT)
+      self.LASTTEST_ROC = LASTTEST_ROC
+  
+
+class Sensor(object):
+  __storm_table__ = "inventory_sensor"
+  SENSOR_ID = Int(primary=True)
+  TRANSFER_ID = Int()
+  transfer = Reference(TRANSFER_ID, Transfer.TRANSFER_ID)
+  COMMENT = Unicode()
+  LASTTEST_SENSOR =Int()
+  def __init__(self,SENSOR_ID,TRANSFER_ID, COMMENT="", LASTTEST_SENSOR=0):
+      self.SENSOR_ID=SENSOR_ID
+      self.TRANSFER_ID=(TRANSFER_ID)
+      self.COMMENT=unicode(COMMENT)
+      self.LASTTEST_SENSOR = LASTTEST_SENSOR
+      
+
+class BareModule(object):
+  __storm_table__ = "inventory_baremodule"
+  BAREMODULE_ID = Unicode(primary=True)
+  ROC_ID =  Unicode() # comma separated list
+  SENSOR_ID = Unicode()
+  TRANSFER_ID = Int()
+  def roc(self,i):
+      internal=self.getRoc(i)
+      print "III",internal
+      rocc=Reference(internal, Roc.ROC_ID)
+      return rocc
+  sensor = Reference(SENSOR_ID, Sensor.SENSOR_ID)
+  transfer = Reference(TRANSFER_ID, Transfer.TRANSFER_ID)
+  BUILTON = date.today()
+  BUILTBY = Unicode()
+  COMMENT = Unicode()
+  LASTTEST_BAREMODULE =Int()
+  def getRoc(self,i):
+      #
+      # parse ROC_IDs to extract field # N
+      #
+      result =(self.ROC_ID.split(","))[i]
+      return result
+  def __init__(self,BAREMODULE_ID,ROC_ID,SENSOR_ID,TRANSFER_ID,  BUILTBY, BUILTON=date.today(),COMMENT="", LASTTEST_BAREMODULE=0):
+      self.BAREMODULE_ID=unicode(BAREMODULE_ID)
+      self.ROC_ID=unicode(ROC_ID)
+      self.SENSOR_ID=unicode(SENSOR_ID)
+      self.TRANSFER_ID=TRANSFER_ID
+      self.BUILTON=BUILTON
+      self.BUILTBY=unicode(BUILTBY)
+      self.COMMENT=unicode(COMMENT)
+      self.LASTTEST_BAREMODULE=LASTTEST_BAREMODULE
+      
+  
+  
+class Hdi(object):
+  __storm_table__ = "inventory_hdi"
+  HDI_ID = Unicode(primary=True)
+  TRANSFER_ID = Int()
+  transfer = Reference(TRANSFER_ID, Transfer.TRANSFER_ID)
+  COMMENT = Unicode()
+  LASTTEST_HDI=Int()
+def __init__(self,HDI_ID, TRANSFER_ID, COMMENT="", LASTTEST_HDI=0):
+    self.HDI_ID=unicode(HDI_ID)
+    self.TRANSFER_ID=TRANSFER_ID
+    self.COMMENT=unicode(COMMENT)
+    self.LASTTEST_HDI=LASTTEST_HDI
+  
+class Tbm(object):
+  __storm_table__ = "inventory_tbm"
+  TBM_ID = Unicode(primary=True)
+  TRANSFER_ID = Int()
+  transfer = Reference(TRANSFER_ID, Transfer.TRANSFER_ID)
+  COMMENT = Unicode()
+  LASTTEST_TBM=Int()
+  def __init__(self,TBM_ID, TRANSFER_ID, COMMENT="", LASTTEST_TBM=0):
+      self.TBM_ID=unicode(TBM_ID)
+      self.TRANSFER_ID=TRANSFER_ID
+      self.COMMENT=unicode(COMMENT)
+      self.LASTTEST_TBM=LASTTEST_TBM
+  
+
+     
+class FullModule(object):
+  __storm_table__ = "inventory_fullmodule"
+  FULLMODULE_ID = Unicode(primary=True)
+  BAREMODULE_ID =  Unicode()
+  HDI_ID =  Unicode()
+  TBM_ID =  Unicode()
+  SENSOR_ID = Unicode()
+  TRANSFER_ID = Int()
+  tbm = Reference(TBM_ID, Tbm.TBM_ID)
+  baremodule = Reference(BAREMODULE_ID, BareModule.BAREMODULE_ID)
+  hdi = Reference(HDI_ID, HDI_ID)
+  BUILTON = date.today()
+  BUILTBY = Unicode()
+  COMMENT = Unicode()
+  LASTTEST_FULLMODULE=Int()
+  def __init__(self,FULLMODULE_ID, BAREMODULE_ID, HDI_ID, TBM_ID, TRANSFER_ID, BUILTBY, BUILTON=date.today(), COMMENT="", LASTTEST_FULLMODULE=0):
+      self.TBM_ID=unicode(TBM_ID)
+      self.HDI_ID=unicode(HDI_ID)
+      self.BAREMODULE_ID=unicode(BAREMODULE_ID)
+      self.FULLMODULE_ID=unicode(FULLMODULE_ID)
+      self.TRANSFER_ID=TRANSFER_ID
+      self.COMMENT=unicode(COMMENT)
+      self.LASTTEST_FULLMODULE=LASTTEST_FULLMODULE
+      self.BUILTON=BUILTON
+      self.BUILTBY=unicode(BUILTBY)
+
+
+#
+# logbook
+#
+class Logbook(object):
+      __storm_table__ = "logbook"
+      ENTRY_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference(SESSION_ID, Session.SESSION_ID)
+      TEST_ROCS = Unicode()
+      TEST_TBMS = Unicode()
+      TEST_HDIS = Unicode()
+      TEST_SENSORS = Unicode()
+      TEST_BAREMODULES = Unicode()
+      TEST_FULLMODULES = Unicode()
+      COMMENT=Unicode()
+      ADDDATA_ID = Int()
+
+#
+# Data
+
+
+class Data(object):
+      __storm_table__ = "test_data"
+      DATA_ID = Int(primary=True)
+      URLs = Unicode()
+      PFNs=Unicode()
+      COMMENT=Unicode()
+
+#
+# tests
+#
+class Test_Baremodule(object):
+      __storm_table__ = "test_baremodule"
+      TEST_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference (SESSION_ID,Session.SESSION_ID)
+      BAREMODULE_ID =  Unicode()
+      baremodule=Reference(BAREMODULE_ID, BareModule.BAREMODULE_ID)
+      RESULT=Float()
+      DATA_ID=Int()
+      data=Reference(DATA_ID,Data.DATA_ID)
+
+class Test_Fullmodule(object):
+      __storm_table__ = "test_baremodule"
+      TEST_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference (SESSION_ID,Session.SESSION_ID)
+      FULLMODULE_ID =  Unicode()
+      fullmodule=Reference(FULLMODULE_ID, FullModule.BAREMODULE_ID)
+      RESULT=Float()
+      DATA_ID=Int()
+      data=Reference(DATA_ID,Data.DATA_ID)
+      ROCSWORSEPERCENT=Float()
+      NOISE=Unicode()
+      TRIMMING =Unicode()
+      PHCAL = Unicode()
+      CURRENT1UA = Float()
+      CURRENT2UA  = Float()
+      CURRENT1501UA = Float() 
+      CURRENT1502UA = Float()
+      IVSLOPE  = Float()
+      TEMPVALUE = Float() 
+      TEMPERROR = Float()
+      TCYCLVALUE = Float()
+      TCYCLERROR = Float()
+      COMMENT= Unicode()
+
+
+class Test_Tbm(object):
+      __storm_table__ = "test_tmb"
+      TEST_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference (SESSION_ID,Session.SESSION_ID)
+      TBM_ID =  Unicode()
+      tbm=Reference(TBM_ID, Tbm.TBM_ID)
+      RESULT=Float()
+      DATA_ID=Int()
+      data=Reference(DATA_ID,Data.DATA_ID)
+
+
+class Test_Hdi(object):
+      __storm_table__ = "test_hdi"
+      TEST_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference (SESSION_ID,Session.SESSION_ID)
+      HDI_ID =  Unicode()
+      hdi=Reference(HDI_ID, Hdi.HDI_ID)
+      RESULT=Float()
+      DATA_ID=Int()
+      data=Reference(DATA_ID,Data.DATA_ID)
+
+class Test_Roc(object):
+      __storm_table__ = "test_roc"
+      TEST_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference (SESSION_ID,Session.SESSION_ID)
+      ROC_ID =  Unicode()
+      roc=Reference(ROC_ID, Roc.ROC_ID)
+      RESULT=Float()
+      DATA_ID=Int()
+      data=Reference(DATA_ID,Data.DATA_ID)
+
+
+class Test_Sensor(object):
+      __storm_table__ = "test_sensor"
+      TEST_ID = Int(primary=True)
+      SESSION_ID=Int()
+      session = Reference (SESSION_ID,Session.SESSION_ID)
+      SENSOR_ID =  Unicode()
+      sensor=Reference(SENSOR_ID, Sensor.SENSOR_ID)
+      RESULT=Float()
+      DATA_ID=Int()
+      data=Reference(DATA_ID,Data.DATA_ID)
+
+#history
+ 
+class History(object):
+    __storm_table__ = "history"    
+    HISTORY_ID = Int(primary=True)
+    TYPE=Unicode()
+    ID=Unicode()
+    TARGET_TYPE=Unicode()
+    TARGET_ID=Unicode()
+    OPERATION=Unicode()
+    DATE=date.today()
+    COMMENT=Unicode()
+    def __init__(self, TYPE, ID, TARGET_TYPE, TARGET_ID, OPERATION, DATE=date.today(), COMMENT=""):
+        self.TYPE=unicode(TYPE)
+        self.ID=unicode(ID)
+        self.TARGET_TYPE=unicode(TARGET_TYPE)
+        self.TARGET_ID=unicode(TARGET_ID)
+        self.OPERATION=unicode(OPERATION)
+        self.DATE=DATE
+        self.COMMENT=unicode(COMMENT)
+    
+# References
+Roc.lasttest_roc = Reference(  Roc.LASTTEST_ROC, Test_Roc.TEST_ID)
+Sensor.lasttest_sensor = Reference(  Sensor.LASTTEST_SENSOR, Test_Sensor.TEST_ID)
+BareModule.lasttest_baremodule = Reference(  BareModule.LASTTEST_BAREMODULE, Test_Baremodule.TEST_ID)
+Hdi.lasttest_hdi = Reference(  Hdi.LASTTEST_HDI, Test_Hdi.TEST_ID)
+Tbm.lasttest_tbm = Reference(  Tbm.LASTTEST_TBM, Test_Tbm.TEST_ID)
+FullModule.lasttest_fullmodule = Reference(  FullModule.LASTTEST_FULLMODULE, Test_Fullmodule.TEST_ID)
+Logbook.adddata = Reference(Logbook.ADDDATA_ID,Data.DATA_ID)
+
+
+
+
+
+
+
+ 
+
+ 
---
+++ cgi-bin/PixelDB.py
@@ -0,0 +1,146 @@
+from Objects import *
+from MySQLdb import *
+from storm.locals import *
+import string
+
+class PixelDBInterface(object) :
+
+      def __init__(self, operator, center, datee =date.today() ) :
+            self.operator = operator
+            self.center =  center
+            self.date = datee
+            
+      def connectToDB(self) :
+            self.database = create_database("mysql://tester:pixels@cmspisa001/test_pixel")
+            self.store = Store(self.database)
+            
+      def insertTransfer(self,transfer):
+            self.store.add(transfer)
+            self.store.commit()
+            return transfer
+    
+      def insertSensor(self,sensor) :
+            self.store.add(sensor)
+            self.store.commit()
+            # log in history
+            self.insertHistory(type="NULL", id=0, target_type="SENSOR", target_id=sensor.SENSOR_ID, operation="INSERT", datee=date.today(), comment="SPERIAMO BENE")
+            return sensor
+      
+      def insertRoc (self, roc):
+            self.store.add(roc)
+            self.store.commit()
+            # log in history
+            self.insertHistory(type="NULL", id=0, target_type="ROC", target_id=roc.ROC_ID, operation="INSERT", datee=date.today(), comment="SPERIAMO BENE")
+            return roc
+
+      def insertTbm (self, tbm):
+            self.store.add(tbm)
+            self.store.commit()
+            # log in history
+            self.insertHistory(type="NULL", id=0, target_type="TBM", target_id=tbm.TBM_ID, operation="INSERT", datee=date.today(), comment="SPERIAMO BENE")
+            return roc
+
+      def insertHdi (self, hdi):
+            self.store.add(hdi)
+            self.store.commit()
+            # log in history
+            self.insertHistory(type="NULL", id=0, target_type="HDI", target_id=hdi.HDI_ID, operation="INSERT", datee=date.today(), comment="SPERIAMO BENE")
+            return roc      
+      
+      def joinRocs(self, arrayofrocids):
+            print"ECCO ", arrayofrocids
+            return string.join(arrayofrocids,",")
+      
+      def assembleBareModule(self, baremodule_id, roc_ids,  sensor_id, builtby, transfer_id=0):
+            #
+            # check if all the objects are already in DB
+            #
+            if (self.isSensorInserted(sensor_id) == False):
+                  print "ERROR: sensor not existing", sensor_id
+            for i in roc_ids:
+                  if (self.isRocInserted(i) == False):
+                        print "ERROR: roc not existing", i
+            print " CHECK DONE"
+            #
+            
+            if (transfer_id ==0):
+                  # creo un transfer
+                  tr = self.insertTransfer(Transfer(SENDER="",RECEIVER=self.operator, ISSUED_DATE= datetime(1970,1,1), RECEIVED_DATE=self.date))
+                  self.insertTransfer(tr)
+                  transfer_id=tr.TRANSFER_ID
+            newbm = BareModule(BAREMODULE_ID=baremodule_id, ROC_ID=self.joinRocs(roc_ids), SENSOR_ID=sensor_id, TRANSFER_ID=transfer_id,BUILTBY=builtby)
+            self.store.add(newbm)
+            self.store.commit()
+            # log in history sensor
+            self.insertHistory(type="SENSOR", id=sensor_id, target_type="BAREMODULE", target_id=baremodule_id, operation="ASSEMBLE", datee=date.today(), comment="SPERIAMO BENE")
+            # log in history rocs
+            for i in roc_ids:
+                  self.insertHistory(type="ROC", id=i, target_type="BAREMODULE", target_id=baremodule_id, operation="ASSEMBLE", datee=date.today(), comment="SPERIAMO BENE")
+            
+            return newbm
+            
+            
+      def insertHistory(self, type, id, target_type, target_id, operation, datee=date.today(), comment=""):
+            newHist=History(type, id, target_type, target_id, operation, datee, comment)
+            self.store.add(newHist)
+            self.store.commit()            
+            return newHist
+
+      def assembleFullModule(self, fullmodule_id, baremodule_id, tbm_id, hdi_id,  builtby, builton=date.today(),comment="",transfer_id=0):
+            #
+            # check if all the objects are already in DB
+            #
+            if (self.isBareModuleInserted(baremodule_id) == False):
+                  print "ERROR: baremodule not existing", baremodule_id
+	    if (self.isTbmInserted(tbm_id) == False):
+                  print "ERROR: tbm not existing",tbm_id 
+            print " CHECK DONE"
+            #
+            if (transfer_id ==0):
+                  # creo un transfer
+                  tr = self.insertTransfer(Transfer(SENDER="",RECEIVER=self.operator, ISSUED_DATE= datetime(1970,1,1), RECEIVED_DATE=self.date))
+                  self.insertTransfer(tr)
+                  transfer_id=tr.TRANSFER_ID
+            newfm = FullModule(FULLMODULE_ID=fullmodule_id, BAREMODULE_ID=baremodule_id, HDI_ID=hdi_id, TBM_ID=tbm_id,TRANSFER_ID=transfer_id, BUILTBY=builtby, BUILTON=builton, COMMENT=comment)
+            self.store.add(newfm)
+            self.store.commit()
+            # log in history 
+            self.insertHistory(type="BAREMODULE", id=baremodule_id, target_type="FULLMODULE", target_id=fullmodule_id, operation="ASSEMBLE", datee=date.today(), comment="SPERIAMO BENE")
+            self.insertHistory(type="HDI", id=hdi_id, target_type="FULLMODULE", target_id=fullmodule_id, operation="ASSEMBLE", datee=date.today(), comment="SPERIAMO BENE")
+            self.insertHistory(type="TBM", id=tbm_id, target_type="FULLMODULE", target_id=fullmodule_id, operation="ASSEMBLE", datee=date.today(), comment="SPERIAMO BENE")
+            return newfm
+            
+#
+# check methods 
+#
+      def isSensorInserted(self, sensor_id):
+            aa = self.store.find(Sensor, Sensor.SENSOR_ID==sensor_id).one()
+            return aa is not None
+
+      def isRocInserted(self, roc_id):
+            temp=unicode(roc_id)
+            aa = self.store.find(Roc, Roc.ROC_ID==temp).one()
+            return aa is not None
+
+      def isTbmInserted(self, tbm_id):
+            temp=unicode(tbm_id)
+            aa = self.store.find(Tbm, Tbm.TBM_ID==temp).one()
+            return aa is not None
+
+      def isHdiInserted(self, hdi_id):
+            aa = self.store.find(Hdi, Hdi.HDI_ID==hdi_id).one()
+            return aa is not None
+
+      def isBareModuleInserted(self, BareModule_id):
+            temp=unicode(BareModule_id)
+            aa = self.store.find(BareModule, BareModule.BAREMODULE_ID==temp).one()
+            return aa is not None
+
+      def isFullModuleInserted(self, FullModule_id):
+            aa = self.store.find(FullModule, FullModule.BAREMODULE_ID==FullModule_id).one()
+            return aa is not None
+#
+#
+#
+             
+      
